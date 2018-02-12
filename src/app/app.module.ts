453af274f1ba11e7d7db9b1648dbe980b84bf632
dasilvafrederic <frederic.dasilva.pro@gmail.com>
2018-02-12 23:23:32 +0100

    First step building

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 import { AppComponent } from './app.component';
+import { ProductComponent } from './product/product.component';
+import { ProductsComponent } from './products/products.component';
+import { ProductsService } from './products.service';
+import { HomeComponent } from './home.component';
+import { AppRoutingModule } from './app-routing.module';
+import { MaterialModule } from './material.module';
 
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    ProductsComponent,
+    ProductComponent,
+    HomeComponent
+],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    AppRoutingModule,
+    MaterialModule
   ],
-  providers: [],
+  providers: [
+    ProductsService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/products.service.spec.ts
@@ -0,0 +1,17 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async, inject } from '@angular/core/testing';
+import { ProductsService } from './products.service';
+
+describe('Service: Products', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [ProductsService]
+    });
+  });
+
+  it('should ...', inject([ProductsService], (service: ProductsService) => {
+    expect(service).toBeTruthy();
+  }));
+});
+
