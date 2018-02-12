7b60d1dc87f0feb389f2d261d7e507f1efedc90c
dasilvafrederic <frederic.dasilva.pro@gmail.com>
2018-02-12 23:45:52 +0100

    refactoring before adding angular-material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,31 +5,20 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 import { AppComponent } from './app.component';
-import { ProductComponent } from './product/product.component';
-import { ProductsComponent } from './products/products.component';
-import { ProductsService } from './products.service';
-import { HomeComponent } from './home.component';
-import { AppRoutingModule } from './app-routing.module';
 import { MaterialModule } from './material.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductsComponent,
-    ProductComponent,
-    HomeComponent
-],
+  ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
-    AppRoutingModule,
-    MaterialModule
+    MaterialModule,
   ],
-  providers: [
-    ProductsService
-  ],
-  bootstrap: [AppComponent]
+  providers: [],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
